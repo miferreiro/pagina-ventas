478010b7dcceeafa860f070cecd814aead0d36fc
miferreiro <miguel.ferreiro.diaz@gmail.com>
2019-04-28 17:13:42 +0200

    Changes to user management of GAE

--- handlers/default_user.py
+++ handlers/default_user.py
@@ -1,12 +1,19 @@
 #coding: utf-8
 import webapp2
 from webapp2_extras import jinja2
-from google.appengine.api import users
+from webapp2_extras import users
 
 class DefaultUserManager(webapp2.RequestHandler):
-    def get(self):
-        pass
 
+    def get(self):
+        user_login = users.users.get_current_user().nickname()
+        login_logout_url = users.users.create_logout_url("/")
+        jinja = jinja2.get_jinja2(app=self.app)
+        template_values = {
+            "login": user_login,
+            "login_logout_url":login_logout_url
+        }
+        self.response.write(jinja.render_template("defaultUser.html", **template_values))
 
 app = webapp2.WSGIApplication([
     ('/default_user', DefaultUserManager)
--- handlers/welcome.py
+++ handlers/welcome.py
@@ -1,47 +1,23 @@
 #coding: utf-8
 import webapp2
-import hashlib
 from webapp2_extras import jinja2
-from model.User import User
-
+from webapp2_extras import users
 
 class WelcomePage(webapp2.RequestHandler):
     def get(self):
-        template_values ={
-        }
 
+        usr = users.users.get_current_user()
+        users.users.is_current_user_admin()  # es admin
+        if usr:
+            login_logout_url = users.users.create_logout_url("/")
+        else:
+            login_logout_url = users.users.create_login_url("/default_user")
+        template_values ={
+            "login_logout_url": login_logout_url,
+            "usr":usr
+        }
         jinja = jinja2.get_jinja2(app=self.app)
         self.response.write(jinja.render_template("index.html", **template_values))
-
-    def post(self):
-
-        user_login = self.request.get("login")
-        user_password = self.request.get("password")
-
-        if len(user_login.strip()) == 0 or len(user_password.strip()) == 0:
-            template_values = {
-                "error": 'Los campos no pueden ser vacios'
-            }
-            jinja = jinja2.get_jinja2(app=self.app)
-            self.response.write(jinja.render_template("index.html", **template_values))
-            return
-
-        usuario = User.query(User.login == user_login and User.password == str(hashlib.new("sha1", user_password)))
-
-        if usuario.count != 0:
-            jinja = jinja2.get_jinja2(app=self.app)
-            template_values = {
-                "login": user_login
-            }
-            self.response.write(jinja.render_template("defaultUser.html", **template_values))
-            return
-        else:
-            template_values = {
-                "error": 'Los campos introducidos no son correctos'
-            }
-            jinja = jinja2.get_jinja2(app=self.app)
-            self.response.write(jinja.render_template("index.html", **template_values))
-            return
 
 app = webapp2.WSGIApplication([
     ('/', WelcomePage)
